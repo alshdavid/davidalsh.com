fbcf4689b907eae470f11ecc784c059537740c44
David Alsh <alshdavid@gmail.com>
2022-07-16 16:26:18 +0700

    Fresh start

--- .github/deploy.bash
+++ .github/deploy.bash
@@ -3,9 +3,9 @@ export AWS_DEFAULT_OUTPUT="yaml"
 export AWS_PAGER=
 export AWS_EC2_METADATA_DISABLED=true
 
-aws s3 rm --recursive --exclude blog s3://alshdavid-web-com-davidalsh-www
+aws s3 rm --recursive --exclude "blog/*" s3://alshdavid-web-com-davidalsh-www
 aws s3 cp --recursive ./dist s3://alshdavid-web-com-davidalsh-www
-aws cloudfront create-invalidation --distribution-id E1RN9EP7R6042I --paths /\* !/blog
+aws cloudfront create-invalidation --distribution-id E1RN9EP7R6042I --paths /\*
 
 # TODO maybe pre-compress the files and upload them?
 # aws s3 cp --recursive --content-encoding br --exclude ".github/**" ./ s3://davidalsh.com/blog
